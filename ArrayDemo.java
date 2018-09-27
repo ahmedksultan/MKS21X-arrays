3e13e0801b7a904a694204a16223c4af3a6192a2
asultan00 <asultan00@stuy.edu>
2018-09-27 05:22:44 -0400

    almost finished 0b; just need to print

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -18,6 +18,17 @@ public class ArrayDemo {
      }
      //0b - 2D array of ints
      public static void printArray(int[][]ary) {
-          int[][] multiarray = new int[3][4];
+          //initializing array
+          int[][] ary = new int[4][4];
+          //filling array w/ values
+          for ( int r = 0; r < 4; r++ ) {
+               for ( int c = 0; c < 4; c++ ) {
+                    ary[r][c] = ( r * 2 ) + (c);
+               }
+          }
+          //printing 2D array
+          for ( int r = 0; r < 4; r++ ) {
+               for ( int c = 0; c < 4; c++ ) {
+                    //???? i forgot how to do this
      }
 }
