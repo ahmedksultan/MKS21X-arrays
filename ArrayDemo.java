19a8c1ff302a77ba28c9cc4f0460b33128344049
asultan00 <asultan00@stuy.edu>
2018-09-28 07:58:35 -0400

    completed 0a, almost 0b

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -23,7 +23,7 @@ public class ArrayDemo {
                }
           }
           //running 0b (marr)
-          //printArray(marr);
+          printArray(marr);
      }
      //0a - 1D array of ints
      public static void printArray(int[] ary) {
@@ -40,7 +40,13 @@ public class ArrayDemo {
           }
      }
      //0b - 2D array of ints
-     /*public static void printArray(int[][] ary) {
-
-     }*/
+     public static void printArray(int[][] ary) {
+          //printing
+          System.out.print("{ ");
+          for (int i = 0; i < ary.length; i++) {
+               for (int j = 0; j < (ary[i]).length; j++) {
+                    System.out.print(ary[i][j] + " ");
+               }
+          }
+     }
 }
