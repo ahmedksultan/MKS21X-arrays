25cd3aea5659588021dfd4a2fefa319d45b9690c
asultan00 <asultan00@stuy.edu>
2018-09-28 00:37:33 -0400

    finished 0a after restarting

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,34 +1,46 @@
 public class ArrayDemo {
      //main method
      public static void main(String[]args) {
-     }
-     //0a - 1D array of ints
-     public static void printArray(int[]ary) {
-          //initializing array
+          //-- Oa - PRINT 1D STRING --
+          //intializing ary
+          int[] ary;
           ary = new int[11];
-          //filling array w/ values
-          for ( int i = 0; i < 11; i++ ) {
+          //filling ary
+          for (int i = 0; i < 11; i++) {
                ary[i] = i * 10;
           }
-          //printing 1D array
-          for ( int i = 0; i < 11; i++ ) {
+          //running 0a (ary)
+          printArray(ary);
+
+          //-- Ob - PRINT 2D STRING --
+          //initializing marr
+          int[][] marr;
+          marr = new int[4][4];
+          //filling marr
+          for (int i = 0; i < 4; i++) {
+               for (int j = 0; j < 4; j++) {
+                    marr[i][j] = i * j;
+               }
+          }
+          //running 0b (marr)
+          //printArray(marr);
+     }
+     //0a - 1D array of ints
+     public static void printArray(int[] ary) {
+          //printing
+          System.out.print("{ ");
+          for (int i = 0; i < ary.length; i++) {
                int b = ary[i];
-               System.out.println(b);
+               if (i < ary.length - 1) {
+                    System.out.print(b + ", ");
+               }
+               else {
+                    System.out.print(b + " }\n");
+               }
           }
      }
      //0b - 2D array of ints
-     public static void printArray(int[][]ary) {
-          //initializing array
-          int[][] ary = new int[4][4];
-          //filling array w/ values
-          for ( int r = 0; r < 4; r++ ) {
-               for ( int c = 0; c < 4; c++ ) {
-                    ary[r][c] = ( r * 2 ) + (c);
-               }
-          }
-          //printing 2D array
-          for ( int r = 0; r < 4; r++ ) {
-               for ( int c = 0; c < 4; c++ ) {
-                    //???? i forgot how to do this
-     }
+     /*public static void printArray(int[][] ary) {
+
+     }*/
 }
